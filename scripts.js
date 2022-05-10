a2ede0308ecebced0db31d82fd002a4e1179c516
gadbm304 <gadbm304@hsd.k12.or.us>
2022-05-10 14:06:31 -0700

    adding js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
-// Test Scripts
-alert("Does this work?")
+var paragraph = document.createElement('P');
+paragraph.textContent = 'The DOM is the bomb!';
+document.body.appendChild(paragraph);
+
+var div = document.createElement('DIV');
+div.style.height = '100%vh';
+document.body.appendChild(div);
+div.appendChild(paragraph);
+div.addEventListener('mousemove', function(event) {
+    console.log(event);
+    var x = event.clientX;
+    var y = event.clientY;
+    div.textContent = x + ' , ' + y;
+    div.style.backgroundColor = 'rgb(' + x + ' , ' + y + ', 100)'
+});
