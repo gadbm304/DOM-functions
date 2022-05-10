683f443ae414946126a2288b03e3f36c96b7cd3b
gadbm304 <gadbm304@hsd.k12.or.us>
2022-05-10 14:47:39 -0700

    adding color moving mouse

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 var paragraph = document.createElement('P');
 paragraph.textContent = 'The DOM is the bomb!';
+// add to the document
 document.body.appendChild(paragraph);
 
 var div = document.createElement('DIV');
-div.style.height = '100%vh';
+div.style.height = '100vh';
+
+//add div to document
 document.body.appendChild(div);
-div.appendChild(paragraph);
+
 div.addEventListener('mousemove', function(event) {
     console.log(event);
     var x = event.clientX;
     var y = event.clientY;
     div.textContent = x + ' , ' + y;
-    div.style.backgroundColor = 'rgb(' + x + ' , ' + y + ', 100)'
+    div.style.backgroundColor = 'rgb(' + x + ', ' + y + ', 100)';
 });
